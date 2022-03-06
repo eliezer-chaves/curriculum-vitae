8115ee3c044979f21558ce5c2636a9ecac5610e7
Eliezer Chaves <chaves.eliezer@outlook.com>
2022-03-05 22:36:54 -0300

    Responsividade e ajustes de css

---
+++ index.js
@@ -0,0 +1,9 @@
+function mudaArrowToWhite(){
+    document.getElementById('arrow').src;
+    document.getElementById('arrow').src = '/curriculum/lib/arrow-white.svg';
+    
+}
+function mudaArrowToBlack(){
+    document.getElementById('arrow').src;
+    document.getElementById('arrow').src = '/curriculum/lib/arrow-black.svg';
+}
